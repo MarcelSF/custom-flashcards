a4affc1172bf610368ce1603a14156c1a4ae11e4
Marcel Fonseca <marcelsf23br@gmail.com>
2024-11-26 09:20:53 +0100

    Describe relations

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,5 @@
 class Deck < ApplicationRecord
   belongs_to :user
+  has_many :deck_flashcards
+  has_many :flashcards, through: :deck_flashcards
 end
--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,5 @@
 class Flashcard < ApplicationRecord
   belongs_to :user
+  has_many :deck_flashcards
+  has_many :decks, through: :deck_flashcards
 end
