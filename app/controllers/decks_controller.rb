b2c7955af37963121a66f42e7777e8d12b2fcb11
Marcel Fonseca <marcelsf23br@gmail.com>
2024-11-29 11:53:52 +0100

    Add new policies, pry-byebug

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,11 +1,26 @@
 class DecksController < ApplicationController
   def index
+    @decks = current_user.decks
   end
 
   def new
+    @deck = Deck.new
+    authorize @deck
   end
 
+  def create 
+    @deck = Deck.new(deck_params)
+    @deck.user = current_user
+    if @deck.save
+      redirect_to deck_path(@deck)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def create  
+  private
+
+  def deck_params
+    params.require(:deck).permit(:name)
   end
 end
